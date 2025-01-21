7336494666cef1cb03882cc16bc9d6601f48f9c6
sudoruiz <renanruiz1227@gmail.com>
2025-01-21 18:00:36 -0300

    feat: figma adjustments

--- src/components/home/TopicCardsDesktop.tsx
+++ src/components/home/TopicCardsDesktop.tsx
@@ -14,18 +14,18 @@ const TopicCardDesktop = ({ number, title, text, textWidth }: TopicCardDesktopPr
   return (
     <div className="relative">
       <img src="images/card.png" className="mt-6 block md:hidden" />
-      <img src="images/desktop-card.png" className="mt-6 hidden md:block" />
+      <img src="images/desktop-card.png" className="mt-12 hidden md:block" />
 
-      <h1 className="absolute left-[32.99px] top-[50.87px] text-[#F2F2F2] font-bold text-[25.56px] md:text-[36px]">
+      <h1 className="absolute left-[32.99px] top-[74.87px] text-[#F2F2F2] font-bold text-[25.56px] md:text-[36px]">
         {number}
       </h1>
 
-      <h1 className="absolute left-[32.99px] top-[118.87px] text-[#F2F2F2] font-rajdhani text-[17.04px] md:text-2xl">
+      <h1 className="absolute left-[32.99px] top-[142.87px] text-[#F2F2F2] font-rajdhani text-[17.04px] md:text-2xl">
         {title}
       </h1>
 
       <p
-        className={`absolute left-[32.99px] top-[179.87px] text-[11.49px] md:text-base text-[#A9A9A9]`}
+        className={`absolute left-[32.99px] top-[203.87px] text-[11.49px] md:text-base text-[#A9A9A9]`}
         style={{
           width: textWidth.base,
         }}

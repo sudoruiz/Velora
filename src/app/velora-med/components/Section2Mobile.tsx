58d2ba5961b31c3ef5331916c1199bf14a7c7542
sudoruiz <renanruiz1227@gmail.com>
2025-01-20 23:20:18 -0300

    feat: lasts pages, velora med and who we are

---
+++ src/app/velora-med/components/Section2Mobile.tsx
@@ -0,0 +1,203 @@
+const Section2Mobile = () => {
+    return (
+        <div className="block md:hidden">
+            <div className="bg-[url('/images/velora-background-mobile.png')] h-[412px] bg-cover absolute inset-0 top-[577px] z-[-1] flex flex-col items-center">
+                <h1 className="text-[#F2F2F2] font-rajdhani leading-[30.62px] text-2xl mt-[64.12px]">
+                    Acelere o atendimento
+                </h1>
+                <div className="w-[279px] h-[254.47px] flex justify-center relative">
+                    <img src="/images/velora-gradient.png" className="w-full h-full" />
+                    <img src="/images/notebook.png" className="w-[257.07px] h-[181.47px] absolute mt-[73.04px]" />
+                    <div className="border border-[#B4B6BB4A] w-[76.55px] h-[30.62px] absolute left-0 top-[66.61px] gradient-background rounded-[2.27px] flex items-center justify-center">
+                        <div className="w-[67.48px] h-[22.54px] flex flex-col justify-between">
+                            <div className="w-[49.07px] flex flex-row gap-[2.25px]">
+                                <img src="/icons/check-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                <h1 className="text-[#F2F2F2] text-[4.54px] font-semibold leading-[4.54px] whitespace-nowrap">
+                                    Consultas realizadas
+                                </h1>
+                            </div>
+                            <div className="flex flex-row justify-between items-end">
+                                <p className="text-[#F2F2F2] font-bold text-[6.8px] leading-[10.21px]">
+                                    51
+                                </p>
+                                <div className="flex flex-row h-[8.8px] items-center gap-[2.69px]">
+                                    <p className="text-[#22B257] text-[4.54px] leading-[5.1px]">
+                                        + 20%
+                                    </p>
+                                    <img src="/icons/up-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border border-[#B4B6BB4A] w-[76.55px] h-[30.62px] absolute right-0 top-[61.26px] gradient-background rounded-[2.27px] flex items-center justify-center">
+                        <div className="w-[67.48px] h-[22.54px] flex flex-col justify-between">
+                            <div className="w-[49.07px] flex flex-row gap-[2.25px]">
+                                <img src="/icons/no-money-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                <h1 className="text-[#F2F2F2] text-[4.54px] font-semibold leading-[4.54px] whitespace-nowrap">
+                                    Valor perdido
+                                </h1>
+                            </div>
+                            <div className="flex flex-row justify-between items-end">
+                                <p className="text-[#F2F2F2] font-bold text-[6.8px] leading-[10.21px]">
+                                    R$ 2.000
+                                </p>
+                                <div className="flex flex-row h-[8.8px] items-center gap-[2.69px]">
+                                    <p className="text-[#FF5F51] text-[4.54px] leading-[5.1px]">
+                                        + 20%
+                                    </p>
+                                    <img src="/icons/down-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border border-[#B4B6BB4A] w-[76.55px] h-[30.62px] absolute right-0 top-[194.04px] gradient-background rounded-[2.27px] flex items-center justify-center">
+                        <div className="w-[67.48px] h-[22.54px] flex flex-col justify-between">
+                            <div className="w-[49.07px] flex flex-row gap-[2.25px]">
+                                <img src="/icons/cancel-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                <h1 className="text-[#F2F2F2] text-[4.54px] font-semibold leading-[4.54px] whitespace-nowrap">
+                                    Consultas canceladas
+                                </h1>
+                            </div>
+                            <div className="flex flex-row justify-between items-end">
+                                <p className="text-[#F2F2F2] font-bold text-[6.8px] leading-[10.21px]">
+                                    51
+                                </p>
+                                <div className="flex flex-row h-[8.8px] items-center gap-[2.69px]">
+                                    <p className="text-[#FF5F51] text-[4.54px] leading-[5.1px]">
+                                        + 20%
+                                    </p>
+                                    <img src="/icons/down-icon.png" className="w-[6.8px] h-[6.8px]" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p className="text-[#F2F2F2] text-center leading-[19.36px] w-[280px]">
+                    e elimine os erros de agendamento com a nossa
+                    solução automatizada.
+                </p>
+            </div>
+            <div className="bg-[url('/images/velora-background-mobile.png')] mt-[375.13px] h-[374px] bg-cover flex flex-col items-center">
+                <div className="mt-[33.05px]">
+                    <h1 className="font-medium leading-[20.42px] text-[#F2F2F2] text-center">
+                        A Velora Med integra
+                    </h1>
+                    <h1 className="font-rajdhani text-2xl leading-[30.62px] text-[#F2F2F2]">
+                        inteligência artificial
+                    </h1>
+                </div>
+                <div className="w-[285px] h-auto relative">
+                    <img src="/images/notebook.png" className="w-[216.16px] h-[152.59px] mt-[17.36px]" />
+                    <img src="/images/notebook-twice.png" className="absolute w-[99.34px] h-[118.87px] left-[58.55px] top-0" />
+                    <div className="w-[100.05px] h-[19.3px] absolute gradient-background flex flex-row items-center gap-[3.19px] justify-center top-[18px] right-[9.28px]">
+                        <img src="/icons/branch-icon.png" className="w-[12.87px] h-[12.87px]" />
+                        <p className="text-[#A9A9A9] leading-[7.01px] text-[5.79px]">
+                            Otimize
+                            o fluxo de pacientes
+                        </p>
+                    </div>
+                    <div className="w-[94.26px] h-[19.3px] absolute gradient-background flex flex-row items-center gap-[3.19px] justify-center top-[59.95px] right-0">
+                        <img src="/icons/clock-icon.png" className="w-[12.87px] h-[12.87px]" />
+                        <p className="text-[#A9A9A9] leading-[7.01px] text-[5.79px]">
+                            Reduzir tempos de espera
+                        </p>
+                    </div>
+                    <div className="w-[100.05px] h-[19.3px] absolute gradient-background flex flex-row items-center gap-[3.19px] justify-center top-[109.36px] right-[19.18px]">
+                        <img src="/icons/team-icon.png" className="w-[12.87px] h-[12.87px]" />
+                        <p className="text-[#A9A9A9] leading-[7.01px] text-[5.79px]">
+                            Melhorar a experiência
+                            geral
+                        </p>
+                    </div>
+                    <img src="/images/velora-logo-mobile.png" className="top-[138.25px] left-[-11.65px]" />
+                </div>
+            </div>
+            <div className="h-[608px] bg-[url('/images/velora-background-mobile.png')] bg-cover flex flex-col items-center">
+                <div className="w-72">
+                    <div className="w-[232px]">
+                        <p className="text-[#A9A9A9] font-medium text-2xl leading-[48px]">
+                            Inteligência artificial
+                        </p>
+                        <h1 className="font-rajdhani text-[32px] leading-[60px] text-[#F2F2F2]">
+                            Pietra
+                        </h1>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <div className="border border-[#FFFFFF33] h-[76px] mr-[7.8px] mt-[30px]" />
+                        <p className="text-[#A9A9A9] text-lg leading-[19.36px] w-[286.77px] mt-8">
+                            Melhora a comunicação com pacientes, ajudando na criação de
+                            campanhas personalizadas e mensagens estratégicas.
+                        </p>
+                    </div>
+                    <div className="mt-[58.54px] relative">
+                        <img src="/images/pietra-card.png" className="w-[209.59px] h-[281.32px] ml-[30.58px]" />
+                        <img src="/images/pietra.png" className="absolute w-[163.18px] h-[278.37px] left-[62.97px] bottom-[2px]" />
+                        <div className="absolute w-[69.15px] h-[30.52px] gradient-background text-[#F2F2F2] text-[8.58px] leading-[10.39px] flex items-center justify-center top-[31.32px]">
+                            <p className="w-[60px]">
+                                Comunicação
+                                com pacientes
+                            </p>
+                        </div>
+                        <div className="absolute w-[71.71px] h-[30.52px] gradient-background text-[#F2F2F2] text-[8.58px] leading-[10.39px] flex items-center justify-center top-[72.91px] right-0 font-bold">
+                            <p className="w-[62.17px]">
+                                Campanha de marketing
+                            </p>
+                        </div>
+                        <div className="absolute w-[71.06px] h-[20.03px] gradient-background text-[#F2F2F2] text-[8.58px] leading-[10.39px] flex items-center justify-center top-[164px]">
+                            <p >
+                                Agendamentos
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="h-[581px] bg-[url('/images/velora-background-mobile.png')] bg-cover flex flex-col items-center">
+                <div className="w-72">
+                    <div className="w-[232px]">
+                        <p className="text-[#A9A9A9] font-medium text-2xl leading-[48px]">
+                            Inteligência artificial
+                        </p>
+                        <h1 className="font-rajdhani text-[32px] leading-[60px] text-[#F2F2F2]">
+                            Camaron
+                        </h1>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <div className="border border-[#FFFFFF33] h-[76px] mr-[7.8px] mt-[30px]" />
+                        <p className="text-[#A9A9A9] leading-[19.36px] w-[270px] mt-8">
+                            Auxilia na análise de dados clínicos, sugerindo diagnósticos, tratamentos
+                            e organizando prontuários de forma automatizada.
+                        </p>
+                    </div>
+                    <div className="mt-[58.54px] relative">
+                        <img src="/images/camaron-card.png" className="w-[210.82px] h-[282.98px] ml-[46.78px]" />
+                        <img src="/images/camaron.png" className="absolute w-[185.01px] h-[275.42px] left-[63.17px] bottom-[2px]" />
+                        <div className="absolute w-[65.24px] h-[20.15px] gradient-background text-[#F2F2F2] text-[8.64px] leading-[10.45px] flex items-center justify-center top-[28.1px] font-bold">
+                            <p>
+                                Diagnósticos
+                            </p>
+                        </div>
+                        <div className="absolute w-[72.13px] h-[20.15px] gradient-background text-[#F2F2F2] text-[8.64px] leading-[10.45px] flex items-center justify-center top-[111.45px] left-[21.64px] font-bold">
+                            <p>
+                                Tratamentos
+                            </p>
+                        </div>
+                        <div className="absolute w-[84.55px] h-[30.7px] gradient-background text-[#F2F2F2] text-[8.64px] leading-[10.45px] flex items-center justify-center top-[28.67px] right-0 font-bold">
+                            <p className="w-[74.96px]">
+                                Analise de dados
+                                clínicos
+                            </p>
+                        </div>
+                        <div className="absolute w-[73.88px] h-[30.7px] gradient-background text-[#F2F2F2] text-[8.64px] leading-[10.45px] flex items-center justify-center top-[195.16px] right-0">
+                            <p className="w-[65px]">
+                                Organização de
+                                prontuário
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Section2Mobile;
